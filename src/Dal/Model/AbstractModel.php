7cb1aacfbbe6ee0a9097ad44169b60d7ac56683a
Christophe Robert <christophe.robert@babblecar.com>
2015-10-14 14:45:50 +0200

    git requireModel strpos => strRpos

--- src/Dal/Model/AbstractModel.php
+++ src/Dal/Model/AbstractModel.php
@@ -262,8 +262,14 @@ abstract class AbstractModel implements JsonSerializable, ServiceLocatorAwareInt
     {
         $class = null;
         $name = (null !== $prefix) ? $prefix : substr($model, strlen(explode('_', $model)[0]) + 7);
+        
+        //print_r($this->array_prefix);
+        //echo "\n name       : ".$name;
         foreach ($this->array_prefix as $k => $ap) {
-            if (strpos($ap, $name) === (strlen($ap)-strlen($name))) {
+            echo "\n\n \$ap       : ".$ap;
+            echo "\n name      : ".$name;
+            if (strrpos($ap, $name) === (strlen($ap)-strlen($name))) {
+                echo "\nGOOD";
                 unset($this->array_prefix[$k]);
                 $class = clone $this->getServiceLocator()->get($model);
                 $class->setPrefix($prefix);
@@ -274,6 +280,7 @@ abstract class AbstractModel implements JsonSerializable, ServiceLocatorAwareInt
             }
         }
 
+        echo "\n=======\n";
         return $class;
     }
 
