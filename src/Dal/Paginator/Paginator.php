08738cfb1fe1a0176cebc4f1c980bcafb9a5cab5
Christophe Robert <crobert@thestudnet.com>
2017-02-05 17:05:30 +0100

    pagination add order of join

--- src/Dal/Paginator/Paginator.php
+++ src/Dal/Paginator/Paginator.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * 
- * 
+ *
+ *
  * TagnCar (http://tagncar.com/)
- * 
+ *
  * Paginator
  *
  */
@@ -93,52 +93,52 @@ class Paginator
     /**
      * Setter Start
      *
-     * @param mixed $s            
+     * @param mixed $s
      * @return \Dal\Paginator\Paginator
      */
     public function setS($s)
     {
         $this->s = $s;
-        
+
         return $this;
     }
 
     /**
      * Setter number Element
      *
-     * @param int $n            
+     * @param int $n
      * @return \Dal\Paginator\Paginator
      */
     public function setN($n)
     {
         $this->n = $n;
-        
+
         return $this;
     }
 
     /**
      * Setter String DateTime
      *
-     * @param int $d            
+     * @param int $d
      * @return \Dal\Paginator\Paginator
      */
     public function setD($d)
     {
         $this->d = $d;
-        
+
         return $this;
     }
 
     /**
      * Setter Page Number
      *
-     * @param int $p            
+     * @param int $p
      * @return \Dal\Paginator\Paginator
      */
     public function setP($p)
     {
         $this->p = $p;
-        
+
         return $this;
     }
 
@@ -149,49 +149,49 @@ class Paginator
      * ["name_field" => "DESC"]
      * "name_field" string if option o exist
      *
-     * @param array|string $c            
+     * @param array|string $c
      * @return \Dal\Paginator\Paginator
      */
     public function setC($c)
     {
         $this->c = $c;
-        
+
         return $this;
     }
 
     /**
      * Setter Order for column
      *
-     * @param unknown $o            
+     * @param unknown $o
      * @return \Dal\Paginator\Paginator
      */
     public function setO($o)
     {
         $this->o = $o;
-        
+
         return $this;
     }
 
     /**
      * Constructor Pagination
      *
-     * @param array|Dal\Db\Sql\Select $select            
-     * @param Adapter|Sql $adapterOrSqlObject            
-     * @param ResultSetInterface $resultSetPrototype            
+     * @param array|Dal\Db\Sql\Select $select
+     * @param Adapter|Sql $adapterOrSqlObject
+     * @param ResultSetInterface $resultSetPrototype
      * @throws \Exception
      */
     public function __construct($select, $adapterOrSqlObject, ResultSetInterface $resultSetPrototype = null)
     {
         $this->select = $select;
-        
+
         if ($adapterOrSqlObject instanceof Adapter) {
             $adapterOrSqlObject = new Sql($adapterOrSqlObject);
         }
-        
+
         if (! $adapterOrSqlObject instanceof Sql) {
             throw new \Exception('$adapterOrSqlObject must be an instance of Zend\Db\Adapter\Adapter or Zend\Db\Sql\Sql');
         }
-        
+
         $this->sql = $adapterOrSqlObject;
         $this->resultSetPrototype = ($resultSetPrototype) ?: new ResultSet();
     }
@@ -206,9 +206,9 @@ class Paginator
         $statement = (null !== $this->c && null !== $this->d) ? $this->getStatementDay() : $this->getStatementPagination();
         $result = $statement->execute((is_array($this->select)) ? $this->select[1] : null);
         $resultSet = clone $this->resultSetPrototype;
-        
+
         $this->result = $resultSet->initialize($result);
-        
+
         return $this->result;
     }
 
@@ -222,15 +222,15 @@ class Paginator
         if (null === $this->n) {
             $this->n = 1;
         }
-        
+
         $date = new \DateTime($this->d, new \DateTimeZone('UTC'));
-        
+
         $date->sub(new \DateInterval(sprintf('P%dD', ($this->p * $this->n) - 1)));
         $start_date = $date->format('Y-m-j');
-        
+
         $date->add(new \DateInterval(sprintf('P%dD', $this->n)));
         $end_date = $date->format('Y-m-j');
-        
+
         if (is_array($this->select)) {
             $query = sprintf('%s WHERE %s BETWEEN %s AND %s', $this->select[0], $this->c, $start_date, $end_date);
             $adt = $this->sql->getAdapter();
@@ -238,10 +238,10 @@ class Paginator
         } else {
             $select = clone $this->select;
             $select->where(new Between($this->c, $start_date, $end_date));
-            
+
             $statement = $this->sql->prepareStatementForSqlObject($select);
         }
-        
+
         return $statement;
     }
 
@@ -258,13 +258,13 @@ class Paginator
         if (null !== $this->c && is_array($this->c)) {
             $co = (reset($this->c) === 'ASC' || reset($this->c) === '>') ? '>' : '<';
             $cc = key($this->c);
-        } else 
+        } else
             if (null !== $this->c && ! is_array($this->c)) {
                 $co = ($this->o === 'ASC' || $this->o === '>') ? '>' : '<';
                 $cc = $this->c;
                 $this->o = [];
             }
-        
+
         if (is_array($this->select)) {
             $query = $this->select[0];
             if (! empty($cc)) {
@@ -273,14 +273,22 @@ class Paginator
             }
             $query = sprintf('%s LIMIT %d OFFSET %d', $query, $this->n, (($this->p - 1) * $this->n));
             $adt = $this->sql->getAdapter();
-            
+
             $statement = $adt->query($query, $adt::QUERY_MODE_PREPARE);
         } else {
             $table = $this->select->getRawState(Select::TABLE);
             $cols = $this->select->getRawState(Select::COLUMNS);
+            $joins = $this->select->getRawState(Select::JOINS);
             $ords = $this->select->getRawState(Select::ORDER) + $this->o;
             $fords = [];
-            
+
+            foreach ($joins as $value) {
+              if(!empty($value['columns'])) {
+                $cols = array_merge($cols,$value['columns']);
+              }
+            }
+
+
             if (count($cols) === 1 && reset($cols) === '*') {
                 foreach ($ords as $ok => $ov) {
                     if(strpos($ok, '.') === false) {
@@ -307,7 +315,7 @@ class Paginator
                     }
                 }
             }
-            
+
             $Select = new Select();
             $Select->columns(array('*'));
             $Select->from(array('original_select' => $this->select));
@@ -320,7 +328,7 @@ class Paginator
                 } else {
                     $cc = $this->checkColumns($cc, $table, $cols);
                 }
-                
+
                 if ($cc !== false) {
                     $Select->where(array($cc . ' ' . $co . ' ?' => $this->s));
                 }
@@ -328,10 +336,10 @@ class Paginator
             $Select->offset((($this->p - 1) * $this->n));
             $Select->limit($this->n);
             $Select->order($fords);
-            
+
             $statement = $this->sql->prepareStatementForSqlObject($Select);
         }
-        
+
         return $statement;
     }
 
@@ -345,46 +353,45 @@ class Paginator
         if ($this->rowCount !== null) {
             return $this->rowCount;
         }
-        
+
         $param = null;
-        
+
         if (is_array($this->select)) {
             $select = $this->select[0];
             $param = $this->select[1];
             $adt = $this->sql->getAdapter();
-            
+
             $statement = $adt->query(sprintf("SELECT COUNT(1) AS c FROM (%s) AS original_select", $select), $adt::QUERY_MODE_PREPARE);
         } else {
             $select = clone $this->select;
             $select->reset(Select::LIMIT);
             $select->reset(Select::OFFSET);
             $select->reset(Select::ORDER);
-            
+
             $countSelect = new Select();
             $countSelect->columns(array('c' => new Expression('COUNT(1)')));
             $countSelect->from(array('original_select' => $select));
-            
+
             $statement = $this->sql->prepareStatementForSqlObject($countSelect);
         }
-        
+
         $result = $statement->execute($param);
         $row = $result->current();
-        
+
         $this->rowCount = $row['c'];
-        
+
         return $this->rowCount;
     }
 
     /**
      * Check Column name
      *
-     * @param string $ok            
+     * @param string $ok
      * @return array
      */
     private function checkColumns($ok, $table, $cols)
     {
         $fords = false;
-        
         foreach ($cols as $ck => $cv) {
             if ($cv instanceof Expression || $cv instanceof Select || is_string($cv)) {
                 if ($ok === $ck) {
@@ -401,7 +408,7 @@ class Paginator
                 }
             }
         }
-        
+
         return $fords;
     }
 }
